f60de10047264211585bc764efdf027d63a949f9
marcmazraany <122973334+marcmazraany@users.noreply.github.com>
2025-08-22 05:47:59 +0300

    Remove API key from test_key.py

--- unit_testing/test_key.py
+++ unit_testing/test_key.py
@@ -1,7 +1,7 @@
 from openai import OpenAI
 
 client = OpenAI(
-  api_key="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"
+  api_key=""
 )
 
 response = client.responses.create(
